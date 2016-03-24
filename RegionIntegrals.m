cfc2507eb4a9b9e5ad7ea1fb451914f4e7dc5354
Nicolas Deutschmann <n.deutschmann@ipnl.in2p3.fr>
2016-03-24 18:08:34 +0100

    NLO expansion function. Still incomplete -> need to find an optimal way of defining the order we want

--- RegionIntegrals.m
+++ RegionIntegrals.m
@@ -25,3 +25,33 @@ LORI[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_]:=
         Return[RegionIntegrals];
         
         ]
+
+
+
+NLORI[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_]:= 
+    Block[
+        {
+            U,F,Dummy,
+            ASYOuput,
+            ExpandedUs,
+            ExpandedFs,
+            RegionsUFs
+        },
+        {U,F,Dummy}=UF[LoopMomenta,Propagators,Replacements];
+        
+        (*ASYSTUFF*)
+        (*TODO*)
+        (*ASYSTUFF*)
+
+        
+        (* Needs the function developped in the Expansion branch *)
+        ExpandedUs=PolyRegionExpand[U,ASYOutput,RegionRules,rho,2];
+        ExpandedFs=PolyRegionExpand[F,ASYOutput,RegionRules,rho,2];
+
+        RegionUFs = Transpose[{ExpandedUs,ExpandedFs}];
+        
+        RegionIntegrals = FeynmanIntegralUF[#[[1]],#[[2]],Powers,Length[LoopMomenta]]&/@RegionUFs;
+
+        Return[RegionIntegrals];
+        
+        ]
