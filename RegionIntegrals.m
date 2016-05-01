96960abf2beced786d5add9e0719e47f0d19a0ce
me <you@example.com>
2016-05-01 17:30:21 +0200

    Added an arbirtrary order Region Expansion function called RegionIntegrals

--- RegionIntegrals.m
+++ RegionIntegrals.m
@@ -1,4 +1,4 @@
-LORI[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_]:= 
+LORI[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_]:=
     Block[
         {
             U,F,Dummy,
@@ -8,27 +8,27 @@ LORI[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_]:=
             RegionsUFs
         },
         {U,F,Dummy}=UF[LoopMomenta,Propagators,Replacements];
-        
+
 
         ASYOutput=AlphaRepExpand[LoopMomenta, Propagators, Replacements,RegionRules,PreResolve -> True];
-	
-        
+
+
         PRERegionRules = RegionRules /. Rule[a_, b_] :> Rule[a,a*rho^(Exponent[b, x])];
 
         ExpandedUs=PolyRegionExpand[U,ASYOutput,PRERegionRules,rho,1];
         ExpandedFs=PolyRegionExpand[F,ASYOutput,PRERegionRules,rho,1];
 
         RegionUFs = Transpose[{ExpandedUs,ExpandedFs}];
-        
+
         RegionIntegrals = FeynmanIntegralUF[#[[1]],#[[2]],Powers,Length[LoopMomenta]]&/@RegionUFs;
 
         Return[RegionIntegrals];
-        
+
         ]
 
 
 
-NLORI[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_]:= 
+NLORI[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_]:=
     Block[
         {
 	    LOExponent,
@@ -43,8 +43,8 @@ NLORI[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_]:=
 	LOExponents = Simplify[Exponent[#,rho]&/@Flatten[PowerExpand/@LORI[LoopMomenta,Propagators,Replacements,Powers,rho,RegionRules]]];
 
         {U,F,Dummy}=UF[LoopMomenta,Propagators,Replacements];
-        
-        
+
+
         ASYOutput=AlphaRepExpand[LoopMomenta, Propagators, Replacements,RegionRules,PreResolve -> True];
 	Scalings=Table[ASYOutputRegion[[1]]/.Table[y[i]->x[i]rho^ASYOutputRegion[[3,i]],{i,Length[ASYOutputRegion[[1]]]}],{ASYOutputRegion,ASYOutput}];
   	RhoRegionRules = RegionRules /. Rule[a_, b_] :> Rule[a,a*rho^(Exponent[b, x])];
@@ -52,9 +52,42 @@ NLORI[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_]:=
 	ScaledFs=Simplify[Table[F/.Scaling,{Scaling,Scalings}]/.RhoRegionRules];
 
         NonZero[rhu_,i_]:=If[i===0,1,rhu^i];
-        
+
      (*   RegionIntegrals =  Table[Simplify[FeynmanIntegralUF[ScaledUs[[i]],ScaledFs[[i]],Powers, Length[LoopMomenta]]/(NonZero[rho,LOExponents[[i]]])],{i,Length[ScaledUs]}];*)
         RegionIntegrals =  Table[Series[FeynmanIntegralUF[ScaledUs[[i]],ScaledFs[[i]],Powers, Length[LoopMomenta]]/(NonZero[rho,LOExponents[[i]]])//PowerExpand//Simplify,{rho,0,1}],{i,Length[ScaledUs]}];
         Return[RegionIntegrals];
-        
+
+        ]
+
+
+
+RegionExpand[LoopMomenta_,Propagators_,Replacements_,Powers_,rho_,RegionRules_,Pow_]:=
+    Block[
+        {
+	    LOExponent,
+            U,F,Dummy,
+            ASYOuput,
+            ExpandedUs,
+            ExpandedFs,
+            RegionsUFs,
+	    RegionIntegrals
+        },
+
+	LOExponents = Simplify[Exponent[#,rho]&/@Flatten[PowerExpand/@LORI[LoopMomenta,Propagators,Replacements,Powers,rho,RegionRules]]];
+
+        {U,F,Dummy}=UF[LoopMomenta,Propagators,Replacements];
+
+
+        ASYOutput=AlphaRepExpand[LoopMomenta, Propagators, Replacements,RegionRules,PreResolve -> True];
+	Scalings=Table[ASYOutputRegion[[1]]/.Table[y[i]->x[i]rho^ASYOutputRegion[[3,i]],{i,Length[ASYOutputRegion[[1]]]}],{ASYOutputRegion,ASYOutput}];
+  	RhoRegionRules = RegionRules /. Rule[a_, b_] :> Rule[a,a*rho^(Exponent[b, x])];
+	ScaledUs=Simplify[Table[U/.Scaling,{Scaling,Scalings}]/.RhoRegionRules];
+	ScaledFs=Simplify[Table[F/.Scaling,{Scaling,Scalings}]/.RhoRegionRules];
+
+        NonZero[rhu_,i_]:=If[i===0,1,rhu^i];
+
+     (*   RegionIntegrals =  Table[Simplify[FeynmanIntegralUF[ScaledUs[[i]],ScaledFs[[i]],Powers, Length[LoopMomenta]]/(NonZero[rho,LOExponents[[i]]])],{i,Length[ScaledUs]}];*)
+        RegionIntegrals =  Table[Series[FeynmanIntegralUF[ScaledUs[[i]],ScaledFs[[i]],Powers, Length[LoopMomenta]]/(NonZero[rho,LOExponents[[i]]])//PowerExpand//Simplify,{rho,0,Pow}],{i,Length[ScaledUs]}];
+        Return[RegionIntegrals];
+
         ]
